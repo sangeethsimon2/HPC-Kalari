ed8d2bc873ea308dcfa52223172c543e6fadaf4e
sangeethsimon <sangeethsimon2@gmail.com>
2024-03-22 10:21:46 +0100

    Use template param to selectively constr comput param

--- src/jacobiSerialImpl.cpp
+++ src/jacobiSerialImpl.cpp
@@ -8,14 +8,14 @@ template<int DIM>
 void JacobiSerialImpl<DIM>::processAndStoreComputationCoefficients(){
               m_diagx = -2.0 + m_Nx*m_Nx/(2.*m_thermalCoeff*m_dt);
               m_diagy = -2.0 + m_Ny*m_Ny/(2.*m_thermalCoeff*m_dt);
-              if constexpr (DIM==3)
-                m_diagz = -2.0 + m_Nz*m_Nz/(2.*m_thermalCoeff*m_dt);
 
               m_weightx = m_thermalCoeff * m_dt/(m_Nx*m_Nx);
               m_weighty = m_thermalCoeff * m_dt/(m_Ny*m_Ny);
 
-              if constexpr (DIM==3)
+              if constexpr (DIM==3){
+                m_diagz = -2.0 + m_Nz*m_Nz/(2.*m_thermalCoeff*m_dt);
                 m_weightz = m_thermalCoeff * m_dt/(m_Nz*m_Nz);
+              }
            }
 
 
@@ -45,7 +45,7 @@ void JacobiSerialImpl<DIM>::updateSolution(float* _ptr2SolutionInitial, float* _
               for(int j=1;j<m_Ny-1;j++){
                 for(int i=1;i<m_Nx-1;i++){
                     errorAtEachPoint = _ptr2SolutionUpdated[i+j*m_Nx+k*m_Nx*m_Ny] - _ptr2SolutionInitial[i+j*m_Nx+k*m_Nx*m_Ny];
-                    std::cout<<" Error At Each point="<<errorAtEachPoint<<"\n";
+                    //std::cout<<" Error At Each point="<<errorAtEachPoint<<"\n";
                     accumulatedErrorPerTimeStep += errorAtEachPoint*errorAtEachPoint;
                     //Copy arrays
                     //TODO: Would a operator overloading in the state class be an efficient option?
@@ -78,7 +78,7 @@ void JacobiSerialImpl<DIM>::updateSolution(float* _ptr2SolutionInitial, float* _
                 for(int j=1;j<m_Ny-1;j++){
                   for(int i=1;i<m_Nx-1;i++){
                     errorAtEachPoint = _ptr2SolutionUpdated[i+j*m_Nx+k*m_Nx*m_Ny] - _ptr2SolutionInitial[i+j*m_Nx+k*m_Nx*m_Ny];
-                    std::cout<<" Error At Each point="<<errorAtEachPoint<<"\n";
+                    //std::cout<<" Error At Each point="<<errorAtEachPoint<<"\n";
                     accumulatedErrorPerTimeStep += errorAtEachPoint*errorAtEachPoint;
                     //Copy arrays
                     //TODO: Would a operator overloading in the state class be an efficient option?
