8b9f5b10b7a8f89c8656acc57cb9fc1351fc4840
sangeethsimon <sangeethsimon2@gmail.com>
2024-03-22 10:23:12 +0100

    templatized kernel and BC
    This version compiles and works for 2D.

--- src/main.cpp
+++ src/main.cpp
@@ -53,36 +53,47 @@ int main(int argc, char **argv){
     initializationObject.initialize(temperature_updated.get(), params->getTotalGridSize(), 0.0);
 
     //Create boundary updater and update boundaries
-    BoundaryCreatorInterface* boundaryUpdater = new DirichletBoundaryCreator(temperature_initial.get()->getState(),
+    //TODO how do I avoid new here and use RAII to delete the memory automatically?
+    BoundaryCreatorInterface* boundaryUpdater= new DirichletBoundaryCreator<2>(temperature_initial.get()->getState(),
     params->getGridSize("x"), params->getGridSize("y"), params->getGridSize("z"), 10.0);
     //Set the boundary condition type (in the background the factory method is called)
     boundaryUpdater->setBoundaryConditionType();
     //Call the update method of the correct boundary type
 
     //Create a Kernel instance
-    Kernel<2> heatKernel(std::make_unique<JacobiSerialImpl<2>>(params->getGridSize("x"), params->getGridSize("y"), params->getGridSize("z"),
-    params->getTimeStep(), params->getConductivityCoeff()));
+    // TODO How do I avoid creating two unique ptrs here and instead do this depending only on the dimensionality?
+    std::unique_ptr<Kernel<2>> heatKernel2D;
+    std::unique_ptr<Kernel<3>> heatKernel3D;
+
+    if(params->getDimensionality()==2){
+      heatKernel2D = std::make_unique<Kernel<2>>(std::make_unique<JacobiSerialImpl<2>>(params->getGridSize("x"), params->getGridSize("y"), 0,
+      params->getTimeStep(), params->getConductivityCoeff()));
+    }
+    else if (params->getDimensionality()==3) {
+      heatKernel3D =  std::make_unique<Kernel<3>>(std::make_unique<JacobiSerialImpl<3>>(params->getGridSize("x"), params->getGridSize("y"), params->getGridSize("z"),
+      params->getTimeStep(), params->getConductivityCoeff()));
+    }
+    else
+        throw std::runtime_error("Invalid value of DIM, unable to initialize kernel\n");
 
     //Instantiate a host timer
     hostTimer hostTimer;
     hostTimer.startClock();
     int iter=0;
-    //while(iter<1)
     do{
       //For dirichlet boundary conditions, call this only once; for other types, call in convergence loop
       boundaryUpdater->updateBoundaries();
 
       //Call the update method
-      heatKernel.updateSolution(temperature_initial.get(), temperature_updated.get());
+      heatKernel2D->updateSolution(temperature_initial.get(), temperature_updated.get());
 
       //Compute error
-      float error = sqrt(heatKernel.computeError());
+      float error = sqrt(heatKernel2D->computeError());
       std::cout<<"Error="<<error<<"\n";
       //Check for convergence
       /* Break if convergence reached or step greater than maxStep */
       if (error<params->getConvergenceTolerance()) break;
       iter++;
-    //}
     }while(true);
     hostTimer.stopClock();
     hostTimer.printElapsedTime();
